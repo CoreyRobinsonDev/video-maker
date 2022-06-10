eca0d01fc39dfee04ec466d2ebfd6949061bfef0
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-06-09 22:58:58 -0400

    added video making functionality

--- index.js
+++ index.js
@@ -13,15 +13,19 @@ const main = async () => {
   spinner.start();
   spinner.text = `Fetching stories from: Reddit.com/r/${subreddit}\n=================================================\n`;
   spinner.color = "orange";
+
   // grabs stories from reddit and saves them in the stories.json file
   // getStories(subreddit);
   
-  // spinner.color = "gray"; 
   // await getTTS();
-  
-  spinner.text = `Making video\n====================\n`; 
-  spinner.color = "blue";
-  await makeVideo();  
+
+  // elements truly randomly exists or not
+  // if this function crashes, retry 
+  try {
+    await makeVideo();  
+  } catch (error) {
+    await makeVideo();  
+  }
   
   spinner.succeed("Done!");
   process.exit(0);
@@ -73,10 +77,11 @@ fs.watch(folder, (eventType = "rename", filename) => {
         if (err) return console.error(err)
       })
 
-      clipsArr.push(newDir);
       count = 0;
       audioNum++;
 
+      // stores file location 
+      clipsArr.push(newDir);
       fs.writeFile("stories.json", JSON.stringify({[`story-${storyNum}`]: clipsArr, stories}), (err) => {if (err) throw err} )
     } else {
       count++;
@@ -87,58 +92,80 @@ fs.watch(folder, (eventType = "rename", filename) => {
 
 // add audio clips to video
 const makeVideo = async () => {
-  const email = process.env.EMAIL;
-  const password = process.env.PASSWORD;
-  const browser = await puppeteer.launch({headless: false, slowMo: 100});
+  spinner.text = `Making video\n===================\n`; 
+  spinner.color = "blue";
+
+  const browser = await puppeteer.launch({headless: false, slowMo: 100, defaultViewport: {width: 1600, height: 900}});
   const page = await browser.newPage();
 
-  await page.goto("https://app.clipchamp.com/");
+  await page.goto("https://veed.io/new");
 
-  spinner.text += "Logging in...\n";
+  spinner.text += "Loading editor...\n";
 
-  await page.waitForSelector("a[href='/login']");
-  await page.click("a[href='/login']");
-  await page.click("button[data-testid='toggle-password']");
-  await page.click("input[type='email']");
-  await page.keyboard.type(email);
-  await page.click("input[type='password']");
-  await page.keyboard.type(password);
-  await page.click("button[type='submit']");
-  
-  await page.waitForSelector("button[data-testid='create-button']");
-  await page.click("button[data-testid='create-button']");
-
-  // await page.click("a[data-step='next']");
-  // await page.click("a[data-step='skip-and-end-flow']");
-  await page.waitForSelector("button[data-testid='add-media']");
-  await page.click("button[data-testid='add-media']");
-
-  spinner.text += "Uploading audio clips...\n";
-
-  const [fileChooser] = await Promise.all([
-    page.waitForFileChooser(),
-    page.click("body > div > nav > div > div > div > p > span > span")
-  ])
-  await fileChooser.accept(stories[`story-${storyNum}`]);
-  await page.waitForTimeout(6000);
-
-  spinner.text += "Adding clips to timeline...\n";
-
-  for (let i = 0; i < clipsArr.length; i++) {
-    await page.click("#panel-all > div > div");
-
-    for (let j = 0; j < (i * 3) + 8; j++) {
-      await page.keyboard.press("Tab");
+  for (let i = 0; i < stories[`story-${storyNum}`].length-37; i++) {
+    // catches an edge case where the robo misses the "+" button
+    try {
+      await page.waitForSelector("span.browse-link");
+    } catch (err) {
+      await page.click("#dropWrapper-timeline > div > button"); 
     }
-    await page.keyboard.press("Enter");
+    
+    const [fileChooser] = await Promise.all([
+      page.waitForFileChooser(),
+      page.click("span.browse-link")
+    ]);
+    await fileChooser.accept([stories[`story-${storyNum}`][i]]);
+    
+    if (i === 0) {
+      spinner.text += "Adding clips to timeline...\n";
+      await page.click("#root > div > div > div > div > div > div > div:nth-child(2) > img");
+      await page.click("#dropWrapper-upload > div > div > div > div > div > div > div > div > div:nth-child(5) > div > div:nth-child(1) > div > div > input", {clickCount: 3});
+      await page.keyboard.type("#000000");
+      await page.click("#videoBox > div > div > div > div > div.move");
+      await page.click("button[aria-label='Delete']");
+      await page.click("button[data-testid='@edit-image/delete-button-confirm']");
+    }
+    
+    await page.waitForSelector("#dropWrapper-timeline > div > button");
+    await page.click("#dropWrapper-timeline > div > button");
   }
-  await page.waitForTimeout(6000);
+
+  spinner.text += "Publishing video...\n";
+
+  await page.click("body > div > div > div > div > div > button");
+  await page.click("input[data-testid='@inline-edit/input']", {clickCount: 3});
+  await page.keyboard.type(`video-${storyNum}`);
+  await page.click("button[data-testid='@header-controls/publish-button']");
+  await page.click("div[data-testid='@component/dropdown/item']");
+  await page.click("#root > div > div > div > div:nth-child(3)");
+  await page.click("button[data-testid='@export/export-button']");
+
+  spinner.text += "Downloading video...";
+
+  await page.waitForSelector("#__next > div > div > div > div > div > div > span");
+  let downloadPercentage = await page.$eval("#__next > div > div > div > div > div > div > span", (el) => el.innerText);
+  spinner.text += downloadPercentage;
+
+  while (downloadPercentage !== "100%") { 
+    downloadPercentage = await page.$eval("#__next > div > div > div > div > div > div > span", (el) => el.innerText);
+    
+    spinner.text = spinner.text.slice(0, -4) + "." + downloadPercentage + "\n";
+  }
+  await page.waitForSelector("#__next > div > div > div > div > button > svg");
+  await page.click("#__next > div > div > div > div > button > svg");
+  await page.waitForSelector("#__next > div > div > div > div > div > div:nth-child(1) > span");
+  await page.click("#__next > div > div > div > div > div > div:nth-child(1) > span");
+
+  await page.waitForTimeout(2000);
+
   await browser.close();
 }
 
 
 // converts text to tts
 const getTTS = async () => {
+  spinner.color = "gray"; 
+
   for (let i = -1; i < stories[storyNum].content.length; i++) {
     if (i === -1) {
       const browser = await puppeteer.launch({headless: false, args: ["--mute-audio"]});
